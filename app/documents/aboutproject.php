cb5c9c4bacaec3dcaee07e7e0c553495d2f77ae7
Cathy Moran Hajo <cathy.hajo@nyu.edu>
2015-08-18 12:40:30 -0400

    Trying to add link to header and splash page for aboutmspp

---
+++ app/documents/aboutproject.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+        "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<center>
+<head>
+	<title>The Public Writings of Margaret Sanger: Web Edition</title> 
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="styles.css" rel="stylesheet" type="text/css" />
+		<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+//-->
+        </script> 
+<script type="text/javascript" src="http://sangerpapers.org/sanger/app/documents/global.js"></script> 
+</head>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<?php
+include("../includes/header.php");
+?>
+
+<!-- content goes in here //-->
+<body>
+</p>
+
+	<div class="inBox">
+<h1 align="center">	About the Project</h1>
+<p align="center">&nbsp;</p>
+<p>The <a href="http://www.nyu.edu/projects/sanger/project/index.php">Margaret Sanger Papers Project</a> (MSPP) is an historical editing project sponsored by the New York University’s <a href="http://library.nyu.edu/">Division of Libraries</a>. Formed by Dr. Esther Katz in 1987 to locate, arrange, edit, research, and publish the papers of the noted birth control pioneer, it was, until 2014, sponsored by  NYU’s <a href="http://history.as.nyu.edu/page/projects">Department of History</a>. For more on the Project see <a href="https://www.nyu.edu/projects/sanger/project/index.php">Margaret Sanger Papers Project</a>.</p>
+<p>&nbsp;</p>
+<h2 align="center">Publications</h2>
+<h3><u>Microfilm</u></h3>
+<p>The MSPP has published a two-series <a href="https://www.nyu.edu/projects/sanger/publications/microfilm.php">microfilm edition</a>: 
+<p><a href="https://www.nyu.edu/projects/sanger/publications/smith_series.php"><em>The Smith College Collections</em></a> -- Work on the <em>Smith College Collections</em> entailed the rearrangement and organization of over 50,000 Sanger documents in the Margaret Sanger collection and seventeen other collections at the Sophia Smith Collection and Smith College Archives.
+<p><a href="https://www.nyu.edu/projects/sanger/publications/collected_documents.php"><em>The Collected Documents Series</em></a> -- Work on the <em>Collected Documents Series</em> included a ten-year international search of over 1,500 archives and private collections, photocopying material and organizing over 9,000 documents for publication.<p>Both series have been published with a printed reel guide that includes an item-level index by University Publications of America, a division of Lexis-Nexis (now <a href="http://cisupa.proquest.com/ws_display.asp?filter=upa_leaf&item_id={045AB6D6-ABC6-439D-ACFB-F7A37718D427}">Pro-Quest</a>).
+<h3><u>Book</u></h3>
+<p>The MSPP is completing the final volume of its four-volume book edition, <a href="file:///C|/Users/Esther%20Katz/Downloads/Book.php"><em>The Selected Papers of Margaret Sanger</em></a>, published by the <a href="http://www.press.uillinois.edu/books/catalog/74xsd6pe9780252074608.html">University of Illinois Press</a>.</p>
+<p>&nbsp;</p>
+
+<p><a href="https://twitter.com/sangerpapers" class="twitter-follow-button" data-show-count="false">Follow @sangerpapers</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>, <a href="https://www.facebook.com/pages/Friends-of-the-Margaret-Sanger-Papers/53274104181"><img src="http://sangerpapers.org/sanger/app/images/FB-logo.png"  /></a>, and read our <img src="http://sangerpapers.org/sanger/app/images/wordpress-logo.png" height="50" /><a href="https://sangerpapers.wordpress.com" >Blog</a></p>
+		
+<!-- Footer //-->	
+		<div id="footer"></div>
+	<p align="center"><hr />All contents copyright &copy; The Margaret Sanger Papers. All rights reserved.
+	Support for this project was provided by the:</p>
+<p>
+	 <a href="http://www.archives.gov/nhprc/"><img src="http://sangerpapers.org/sanger/app/images/nhprc-logo.jpg" height="50" /></a>
+	 <a href="http://www.neh.gov"><img src="http://sangerpapers.org/sanger/app/images/neh-logo.jpg" height="50" /></a>
+	 <a href="http://www.nyu.edu"><img src="http://sangerpapers.org/sanger/app/images/nyu-logo.gif" height="50" /></a>
+	 </p>
+
+
+</body>
+
+	
+	
+  
+  </div>
+	    </div>
+	</div>
+		
+
+</html>
+
+</div>
+</div>
+				
+		<hr>
+		
+	
