b50b41f44a6c13210716160a0153592923a098d7
ESTHER KATZ <esther.katz@nyu.edu>
2015-11-10 17:48:00 -0500

    updates

---
+++ app/documents/About-Edition.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+        "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<center>
+<head>
+	<title>The Public Writings of Margaret Sanger: Web Edition</title> 
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="http://sangerpapers.org/sanger/app/documents/styles.css" rel="stylesheet" type="text/css" />
+		<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+//-->
+        </script> 
+<script type="text/javascript" src="http://sangerpapers.org/sanger/app/documents/global.js"></script> 
+</head>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+        "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+	<title>The Public Writings of Margaret Sanger: Web Edition</title> 
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<link href="file:///C|/Users/Esther%20Katz/Dropbox/Digital%20Front%20Matter/styles.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="http://sangerpapers.org/sanger/app/documents/global.js"></script> 
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-66075818-1', 'auto');
+ga('send', 'pageview');
+
+</script>
+</head>
+<body onload="MM_preloadImages('file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c1_f2.gif','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c2_f2.gif','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c3_f2.gif','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c4_f2.gif','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c5_f2.gif')">
+<div id="container" style="width: 626px; margin: 0px auto;"> 
+<p align="center" style="margin-top: 0px; margin-bottom: 0px;">
+	<img id="header_r1_c1" src="http://sangerpapers.org/sanger/app/images/header_r1_c1.gif" width="625" height="95" alt="" /><img src="http://sangerpapers.org/sanger/app/images/spacer.gif" width="1" height="95" alt="" /> 
+</p>
+<p align="center" style="margin-top: 0px; /*\*/margin-top: -3px;/**/">
+	<a href="http://sangerpapers.org/sanger/app/documents/aboutproject.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c1','','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c1_f2.gif',1)">
+	<img id="header_r2_c1" src="http://sangerpapers.org/sanger/app/images/header_r2_c1.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/documents/electroniced.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c2','','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c2_f2.gif',1)">
+	<img id="header_r2_c2" src="http://sangerpapers.org/sanger/app/images/header_r2_c2.gif" width="104" height="20" alt="" /></a><a href="http://sangerpaper.org/sanger/documents/search.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c3','','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c3_f2.gif',1)">
+	<img id="header_r2_c3" src="http://sangerpapers.org/sanger/app/header_r2_c3.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c4','','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c4_f2.gif',1)">
+	<img id="header_r2_c4" src="http://sangerpapers.org/sanger/app/images/header_r2_c4.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/contactus/index.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c5','','file:///C|/Users/Esther%20Katz/Dropbox/images/header_r2_c5_f2.gif',1)">
+	<img id="header_r2_c5" src="http://sangerpapers.org/sanger/app/images/header_r2_c5.gif" width="104" height="20" alt="" /></a>
+	<img id="header_r2_c6" src="http://sangerpapers.org/sanger/app/images/header_r2_c6.jpg" width="105" height="20" alt="" /><img src="http://sangerpapers.org/sanger/app/images/spacer.gif" width="1" height="20" alt="" /> 
+</p>
+<div class="outBox">
+	<div class="inBox">
+<body>
+</p>
+
+	<div class="inBox">
+<h1 align="center">About the Edition</h1>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<h3>The Collection</h3></head>
+<body>
+<p><em>The Speeches and Articles of Margaret Sanger (1911-1959)</em>, is a web-based edition of Sanger’s  writings, including articles, pamphlets, speeches, stories, and interviews, 
+produced by <a href="http://www.nyu.edu/projects/sanger/index.php"> The Margaret Sanger Papers Project (MSPP). This collection of papers, speeches, interviews, articles and other writings by birth control reformer Margaret Sanger (1879-1966), is drawn from materials
+collected by the staff of the <a href="http://www.nyu.edu/projects/sanger/index.php"> Margaret Sanger Papers Project (MSPP) in the course of preparing a 2-series microfilm, <em>The <a href="https://www.nyu.edu/projects/sanger/publications/microfilm.php">Margaret 
+Sanger Papers Project Microfilm:</a> Smith College Collection</em> and <em>Collected Documents</em> published in 1996 and 1997), the Library of Congress microfilm of Sanger’s papers, and 
+the MSPP’s four-volume book edition, <a href="http://www.nyu.edu/projects/sanger/publications/book.php"><em>The Selected Papers of Margaret Sanger</em></a> published by Illinois University Press.</a></p>
+<p>Unlike much of the historical material currently available on the Internet, this edition is a completely vetted, historically accurate digital version of her writings and speeches that conform to established standards, 
+both in terms of technical features of its encoding, and providing accurate renderings of the texts. It  provides open access to complete versions of those articles, speeches and statements, most frequently cited and those that are often inaccessible to general 
+readers.  In this digital edition, the Project seeks to make these documents much more accessible to researchers.</p>
+<p>In a collection where there is so much material on a handful of subjects, readers may search documents by date ranges, publication source, and 
+type of document (speech, draft article, etc.), as well as by simple word.  While such searches often return too many matches, they are invaluable for a researcher who wants to see each instance of Sanger’s use of a word or term.  A reader could search for the word "abortion" and go through each of them to get 
+a sense of her views.  However, in such a search will pick up not just discussions of abortion, but mere use of the word.  In many instances Sanger simply said that birth control would reduce the frequency of 
+abortions.  To solve the problem of getting too many, often not relevant “hits,” the Project has prepared a subject index. In the above example, this index will help generate fewer, but more relevant results.</p>
+<p>After eliminating multiple versions , the editors have included nearly 1,000 documents, each verified by date and title. We have searched for published versions when we only have drafts, and have culled from multiple versions to create the most 
+complete documents. Each document was then carefully  transcribed , proofread, and encoded each document, employing the XML P-5 encoding system developed for the humanities by the TEI (Text Encoding Initiative). Documents 
+were then indexed by subject.</p>
+
+<h3>Transcription</h3>
+
+<p>We have transcribed the documents staying as close to the original copy text as possible, while still making the documents easily readable.  
+Changes, amendations, and corrections to the document made by Sanger have been retained , as has most punctuation. Slips of the pen, spelling errors, and other elements 
+that would impede word searches have been silently corrected.  Any text added to the document by the editors (supplied dates, our reading of illegible or partially obscured words) is rendered in [square brackets and italics]. 
+Source notes at the top of each transcription provides information regarding the location or original publication information. The location on our microfilm edition is cited as 
+follows: LCM (Library of Congress microfilm), MSM (“S” for Smith College Collections and “C”Collected Documents), followed by the reel and frame numbers.</p>
+<p>Please note: These transcriptions have been mounted for research purposes only and the Sanger Project requests that Sanger Project URL be used when referring to any of these materials. 
+Links may be made to our site (though we wish to be notified), but under no circumstances are the transcriptions to be copied and mounted onto another server without express permission.</p>
+<h3>Encoding</h3>
+<p></p>
+</body>
+</html>
